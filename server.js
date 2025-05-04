d274fbf729e9955c19c762e678783c53f713718a
Munnangi-Divya <divyamunnangi41@gmail.com>
2025-05-05 00:53:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/customers', require('./routes/customerRoutes'));
 app.use('/api/loans', require('./routes/loanRoutes'));
 app.use('/api/repayments', require('./routes/repaymentRoutes'));
+app.get('/', (req, res) => {
+  res.send('CrediKhaata API is running!');
+});
+
 
 mongoose.connect(process.env.MONGO_URI).then(() => {
   app.listen(process.env.PORT || 5000, () => console.log('Server running'));
